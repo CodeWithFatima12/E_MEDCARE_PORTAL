11cbcbd7134a51425fa4a68b692684be65192bb7
CodeWithFatima12 <ba5807481@gmail.com>
2026-04-19 22:19:17 +0300

    Initial commit: AI Powered E-Medcare setup

---
+++ AI_POWERED_EMEDCARE_PORTAL/__init__.py
@@ -0,0 +1,7 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# import pymysql
+
+# pymysql.version_info = (2, 2, 8, "final", 0)  # This tricks Django's version check
+# pymysql.install_as_MySQLdb()
